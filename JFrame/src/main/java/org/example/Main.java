f2a089974e231b65c9911f3d0f2e4819c580c570
S4v!0r <gbp17@outlook.com>
2023-08-27 23:34:40 -0500

    Add setLocationRelativeTo(null)

--- JFrame/src/main/java/org/example/Main.java
+++ JFrame/src/main/java/org/example/Main.java
@@ -3,6 +3,7 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
         Ventana ventana = new Ventana();
+        ventana.setLocationRelativeTo(null);
         ventana.setVisible(true);
     }
 }
